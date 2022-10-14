a0553dcdd5f408b0c9671f118d78c119c03ce6cf
vadim.korelin <vadim.korelin@mati.io>
2022-10-14 16:05:00 +0300

    react demo event listeners

--- react-javascript/src/App.js
+++ react-javascript/src/App.js
@@ -1,6 +1,20 @@
+import { useRef, useEffect } from 'react';
+
 function App() {
+  const button = useRef(null);
+
+  useEffect(() => {
+    if (button.current) {
+      button.current.addEventListener('metamap:loaded', ({ detail }) => console.log('[METAMAP] Loaded', detail));
+      button.current.addEventListener('metamap:userStartedSdk', ({ detail }) => console.log('[METAMAP] User started verification', detail));
+      button.current.addEventListener('metamap:userFinishedSdk', ({ detail }) => console.log('[METAMAP] User finished verification', detail));
+      button.current.addEventListener('metamap:exitedSdk', ({ detail }) => console.log('[METAMAP] User exited verification', detail));
+    }
+  }, [button]);
+
   return (
     <metamap-button
+      ref={button}
       clientid="<clientid>"
       flowid="<flowid>"
     >

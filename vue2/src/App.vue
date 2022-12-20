13f28ad1a44472a040a137f8a4a16febeb0a8e53
anatoliy.turkin <anatoliy.turkin@mati.io>
2022-12-20 13:20:12 +0300

    add vue 2 demo app

---
+++ vue2/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    <MetamapButton />
+  </div>
+</template>
+
+<script>
+import MetamapButton from './components/MetamapButton.vue'
+import Vue from 'vue'
+
+Vue.config.ignoredElements = [
+  'metamap-button',
+]
+
+export default {
+  name: 'App',
+  components: {
+    MetamapButton
+  }
+}
+</script>
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.app {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
